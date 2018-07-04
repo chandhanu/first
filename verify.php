9dfd6f09d82fdeac5467c2f3612ab906fd67aed1
Chan <chandhanu.m.k@gmail.com>
2018-07-04 12:55:08 +0530

    first website

---
+++ verify.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<p align="Center">
+Name:<?php echo $_POST ["name"]; ?><br><br>
+Email:<?php echo $_POST ["email"]; ?><br><br>
+Contact number:<?php echo $_POST ["phone"]; ?><br><br>
+Date of birth:<?php echo $_POST ["dob"]; ?> <br><br>
+Gender:<?php echo $_POST ["gender"]; ?><br></p>
+</body>
+</html>
